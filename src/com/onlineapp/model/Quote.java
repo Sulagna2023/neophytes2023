0db8b399809434846b535aed388dc7fd82245035
saurabhdhote2812 <142255630+saurabhdhote2812@users.noreply.github.com>
2023-09-08 21:24:13 +0530

    layered architecture
    added a quote model class

---
+++ src/com/onlineapp/model/Quote.java
@@ -0,0 +1,58 @@
+package com.onlineapp.model;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+public class Quote {
+	
+	LocalDateTime orderDate;
+	Customer customer;
+	double shippingCost;
+	List<Products> productList;
+	double totalOrderValue;
+	
+	Quote(){
+	}
+
+	public LocalDateTime getOrderDate() {
+		return orderDate;
+	}
+
+	public void setOrderDate(LocalDateTime orderDate) {
+		this.orderDate = orderDate;
+	}
+
+	public double getShippingCost() {
+		return shippingCost;
+	}
+
+	public void setShippingCost(double shippingCost) {
+		this.shippingCost = shippingCost;
+	}
+
+	public List<Products> getProductList() {
+		return productList;
+	}
+
+	public void setProductList(List<Products> productList) {
+		this.productList = productList;
+	}
+
+	public double getTotalOrderValue() {
+		return totalOrderValue;
+	}
+
+	public void setTotalOrderValue(double totalOrderValue) {
+		this.totalOrderValue = totalOrderValue;
+	}
+
+	@Override
+	public String toString() {
+		return "Quote [orderDate=" + orderDate + ", customer=" + customer + ", shippingCost=" + shippingCost
+				+ ", productList=" + productList + ", totalOrderValue=" + totalOrderValue + "]";
+	}
+	
+	
+	
+
+}
