23548e9e841fb6b388dd58e6de464767060a2731
Falguni Jawalkar <falgunijawalkar@gmail.com>
2023-09-10 10:44:44 +0530

    invoice added

---
+++ frontend/js/script.js
@@ -0,0 +1,7 @@
+function redirectToInvoice() {
+    window.location.href = "../html/invoice.html"; 
+}
+
+function redirectToApproved(){
+    window.location.href = "../html/approveAction.html"; 
+}
